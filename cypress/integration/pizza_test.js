dc036119768e196450000a5ad802c6237e190cc6
Mary Baker <meb601@gmail.com>
2020-08-21 14:50:06 -0400

    Reconfigure Cypress, add cy.visit, re-order tests, add test for drop-down

--- cypress/integration/pizza_test.js
+++ cypress/integration/pizza_test.js
@@ -1,7 +1,17 @@
 describe('Pizza App', () => {
+
+ 
+
     describe('Inputs and submit button', () => {
+      
       it('the submit button is disabled', () => {
+        cy.visit('http://localhost:3000') 
         cy.get('.submit-button').should('be.disabled')
+      })
+
+      it('can select size drop-down', () => {
+        cy.get('select').select('Small') 
+          .should('have.value', 'small')
       })
   
       it('can type something in the "name" input', () => {
@@ -15,13 +25,13 @@ describe('Pizza App', () => {
           .type('Testing testing 123')
           .should('have.value', 'Testing testing 123')
       })
+
+      it('Boxes can be checked and unchecked', () => {
+        cy.get('[type="checkbox"]').check()
+      })
   
       it('submit button is enabled', () => {
         cy.get('.submit-button').should('be.enabled')
-      })
-      
-      it('submit button is enabled', () => {
-        cy.get('[type="checkbox"]').check()
       })
       
     })
