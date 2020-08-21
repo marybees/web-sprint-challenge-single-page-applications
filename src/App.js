6525355b77376a22150418f6e57c33458c06449f
Mary Baker <meb601@gmail.com>
2020-08-21 11:52:23 -0400

    Draft App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,126 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import Pizza from './Pizza'
+import PizzaForm from './PizzaForm'
+import formSchema from './formSchema'
+import axios from 'axios'
+import * as yup from 'yup'
 
-const App = () => {
+//////////////// INITIAL STATES ////////////////
+//////////////// INITIAL STATES ////////////////
+//////////////// INITIAL STATES ////////////////
+const initialFormValues = {
+  name: '',
+  size: '',
+  toppings: {
+    hiking: false,
+    reading: false,
+    coding: false,
+  },
+  instructions: '',
+}
+const initialFormErrors = {
+  name: '',
+  size: '',
+  toppings: '',
+  instructions: '',
+}
+const initialPizzas = []
+const initialDisabled = true
+
+
+export default function App() {
+  const [pizzas, setPizzas] = useState(initialPizzas)          // array of pizza objects
+  const [formValues, setFormValues] = useState(initialFormValues) // object
+  const [formErrors, setFormErrors] = useState(initialFormErrors) // object
+  const [disabled, setDisabled] = useState(initialDisabled)       // boolean
+
+  const postNewPizza = newPizza => {
+    axios.post('https://reqres.in/api/pizzas', newPizza)
+      .then(res => {
+        setUsers([...pizzas, res.data])
+      })
+      .catch(err => {
+        return err
+      })
+      .finally(() => {
+        setFormValues(initialFormValues)
+      })
+  }
+
+  const inputChange = (name, value) => {
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then(valid => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        })
+      })
+      .catch(err => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        })
+      })
+
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const checkboxChange = (name, isChecked) => {
+    setFormValues({
+      ...formValues,
+      toppings: {
+        ...formValues.toppings,
+        [name]: isChecked,
+      }
+    })
+  }
+
+  const submit = () => {
+    const newPizza = {
+      name: formValues.name.trim(),
+      size: formValues.size,
+      toppings: Object.keys(formValues.toppings).filter(topping => formValues.toppings[topping]),
+      instructions: formValues.instructions.trim(),
+    }
+    postNewPizza(newPizza)
+  }
+
+  // useEffect(() => {
+  //   getPizzas()
+  // }, [])
+
+  useEffect(() => {
+    formSchema.isValid(formValues)
+      .then(valid => {
+        setDisabled(!valid);
+      })
+  }, [formValues])
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
-};
-export default App;
+    <div className='container'>
+      <header><h1>Lambda Eats</h1></header>
+
+      <PizzaForm
+        values={formValues}
+        inputChange={inputChange}
+        checkboxChange={checkboxChange}
+        submit={submit}
+        disabled={disabled}
+        errors={formErrors}
+      />
+
+      {
+        pizzas.map(pizza => {
+          return (
+            <Pizza key={pizza.id} details={pizza} />
+          )
+        })
+      }
+    </div>
+  )
+}
---
+++ src/PizzaForm.js
@@ -0,0 +1,145 @@
+// import React from 'react'
+
+// export default function FriendForm(props) {
+//   const {
+//     values,
+//     submit,
+//     inputChange,
+//     checkboxChange,
+//     disabled,
+//     errors,
+//   } = props
+
+//   const onSubmit = evt => {
+//     evt.preventDefault()
+//     submit()
+//   }
+
+//   const onCheckboxChange = evt => {
+//     const { name, checked } = evt.target
+//     checkboxChange(name, checked)
+//   }
+
+//   const onInputChange = evt => {
+//     const { name, value } = evt.target
+//     inputChange(name, value)
+//   }
+
+//   return (
+//     <form className='form container' onSubmit={onSubmit}>
+//       <div className='form-group submit'>
+//         <h2>Add a Friend</h2>
+
+//         {/* 🔥 DISABLE THE BUTTON */}
+//         <button disabled={disabled}>submit</button>
+
+//         <div className='errors'>
+//           {/* 🔥 RENDER THE VALIDATION ERRORS HERE */}
+//           <div>{errors.username}</div>
+//           <div>{errors.email}</div>
+//           <div>{errors.role}</div>
+//           <div>{errors.civil}</div>
+//         </div>
+//       </div>
+
+//       <div className='form-group inputs'>
+//         <h4>General information</h4>
+
+//         {/* ////////// TEXT INPUTS ////////// */}
+//         {/* ////////// TEXT INPUTS ////////// */}
+//         {/* ////////// TEXT INPUTS ////////// */}
+//         <label>Username&nbsp;
+//           <input
+//             value={values.username}
+//             onChange={onInputChange}
+//             name='username'
+//             type='text'
+//           />
+//         </label>
+
+//         <label>Email
+//           <input
+//             value={values.email}
+//             onChange={onInputChange}
+//             name='email'
+//             type='text'
+//           />
+//         </label>
+
+//         {/* ////////// DROPDOWN ////////// */}
+//         {/* ////////// DROPDOWN ////////// */}
+//         {/* ////////// DROPDOWN ////////// */}
+//         <label>Role
+//           <select
+//             onChange={onInputChange}
+//             value={values.role}
+//             name='role'
+//           >
+//             <option value=''>- Select an option -</option>
+//             <option value='student'>Student</option>
+//             <option value='alumni'>Alumni</option>
+//             <option value='instructor'>Instructor</option>
+//             <option value='tl'>Team Lead</option>
+//           </select>
+//         </label>
+
+//         {/* ////////// RADIO BUTTONS ////////// */}
+//         {/* ////////// RADIO BUTTONS ////////// */}
+//         {/* ////////// RADIO BUTTONS ////////// */}
+//         <label>Single
+//           <input
+//             type="radio"
+//             name='civil'
+//             value='single'
+//             checked={values.civil === 'single'}
+//             onChange={onInputChange}
+//           />
+//         </label>
+
+//         <label>Married
+//           <input
+//             type="radio"
+//             name="civil"
+//             value='married'
+//             checked={values.civil === 'married'}
+//             onChange={onInputChange}
+//           />
+//         </label>
+//       </div>
+
+//       <div className='form-group checkboxes'>
+//         <h4>Hobbies</h4>
+
+//         {/* ////////// CHECKBOXES ////////// */}
+//         {/* ////////// CHECKBOXES ////////// */}
+//         {/* ////////// CHECKBOXES ////////// */}
+//         <label>Hiking
+//           <input
+//             type="checkbox"
+//             name='hiking'
+//             checked={values.hobbies.hiking}
+//             onChange={onCheckboxChange}
+//           />
+//         </label>
+
+//         <label>Reading
+//           <input
+//             type="checkbox"
+//             name="reading"
+//             checked={values.hobbies.reading}
+//             onChange={onCheckboxChange}
+//           />
+//         </label>
+
+//         <label>Coding
+//           <input
+//             type="checkbox"
+//             name="coding"
+//             checked={values.hobbies.coding}
+//             onChange={onCheckboxChange}
+//           />
+//         </label>
+//       </div>
+//     </form>
+//   )
+// }
